bea7afbc007c79d973f58aa1c5fb5e38276619ea
Michael Hefferan <michael.hefferan@mydrivesolutions.com>
2016-07-13 11:22:56 +0100

    Remove sprockets test

--- lib/leaflet-rails.rb
+++ lib/leaflet-rails.rb
@@ -10,10 +10,8 @@ module Leaflet
   module Rails
     class Engine < ::Rails::Engine
       initializer 'leaflet-rails.precompile' do |app|
-        if using_sprockets4?
-          app.config.assets.precompile += %w(layers-2x.png layers.png marker-icon-2x.png marker-icon.png marker-shadow.png)
-          app.config.assets.precompile += %w(markers-matte.png markers-matte@2x.pnd markers-plain.png markers-shadow.png markers-soft.png markers-soft@2x.png)
-        end
+        app.config.assets.precompile += %w(layers-2x.png layers.png marker-icon-2x.png marker-icon.png marker-shadow.png)
+        app.config.assets.precompile += %w(markers-matte.png markers-matte@2x.pnd markers-plain.png markers-shadow.png markers-soft.png markers-soft@2x.png)
       end
 
       initializer 'leaflet-rails.helpers' do
